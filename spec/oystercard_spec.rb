c848315cad333091b0e11fdaa8377505739dd8ee
RumenKanev <rkynev@gmail.com>
2021-10-18 20:07:00 +0100

    05_top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,18 @@ require 'oystercard'
 
 describe Oystercard do
 
-  describe '#balance' do 
+  describe "#balance" do 
     it "has a balance of zero" do 
       expect(subject.balance).to eq (0)
     end
+  end
+  describe "#top-up" do 
+    it "cheks for top up functuanality" do 
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      end
   end
 
 end
