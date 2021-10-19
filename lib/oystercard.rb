753f173b8cbe52a2c45511a3fe412024c040ef74
RumenKanev <rkynev@gmail.com>
2021-10-19 12:54:05 +0100

    08_touch_in_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,11 @@
 class Oystercard
     MAXIMUM_BALANCE = 90
-    attr_reader :balance
+    attr_reader :balance, :in_journey
+    alias :in_journey? :in_journey
 
     def initialize
         @balance = 0
+        @in_journey = false
     end
 
     def top_up(cash)
@@ -17,5 +19,14 @@ class Oystercard
     def deduct(cash)
         @balance -= cash
     end
+
+    def touch_in
+        @in_journey = true
+    end
+
+    def touch_out
+        @in_journey = false
+    end
+    
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -29,5 +29,19 @@ describe Oystercard do
       expect{subject.deduct 1 }.to change{subject.balance}.by -1
     end
   end
+  describe "#in journey" do 
+    it 'is initially not in a journey' do
+      expect(subject).not_to be_in_journey
+    end
+    it "can touch in" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+      end
+    it "can touch out" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
 
 end
