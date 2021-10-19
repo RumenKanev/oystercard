763a6546d1cd42d959bf0a5706888106ad7bb644
RumenKanev <rkynev@gmail.com>
2021-10-19 09:20:38 +0100

    06_maximum_balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,5 @@
 class Oystercard
+    MAXIMUM_BALANCE = 90
     attr_reader :balance
 
     def initialize
@@ -6,6 +7,11 @@ class Oystercard
     end
 
     def top_up(cash)
-        @balance += cash
+        if (cash + balance) > MAXIMUM_BALANCE
+          fail "Maximum balance of 90 exceeded" 
+        else
+          @balance += cash
+        end
     end
 end
+
